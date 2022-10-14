8222f616b19379247e441b518be99b7b5d8ef20f
An-Re-16 <anjalianre16@gmail.com>
2022-10-14 12:19:06 +0530

    new

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -16,6 +16,16 @@ const About = () => {
       >
         <h2 style={{ marginLeft: "15px" }}>About</h2>
       </div>
+      <h3>OUR NUTRI FOOD </h3><h4><subscript>YOUR DAILY NEED PRODUCTS</subscript></h4> 
+        <p>
+        <pre>
+        Greetings from Nutri Pure. Our software is committed to educating you about the truth behind the trends so you can concentrate on improvement rather than perfection. We respond to your most common inquiries about nutrition and fitness with verified, factual responses.
+
+We serve as a dependable source of current knowledge on all aspects of fitness and nutrition that are important to your health and vitality. We put a lot of effort into developing and perfecting our 5000+ collection of well curated, thorough, fact-checked material.
+
+The goal of the Nutri Pure Review Board is to provide you with practical, science-based recommendations that will motivate you to make healthy daily choices.
+        </pre>
+        </p>
         </div>
     );
 };
