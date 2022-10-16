c9d45a9a9b4239e3f74ef580dcb6b2ec31f8c0f8
Krishna-vamsi-21 <krishnavamsiyadavbonigi@gmail.com>
2022-10-16 22:14:19 +0530

    Final push

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -13,10 +13,12 @@ const About = () => {
           alignItems: "center",
         }}
       >
+
         <h2 style={{ marginLeft: "15px" }}>About</h2>
       </div>
-      <h3>OUR NUTRI FOOD </h3><h4><subscript>YOUR DAILY NEED PRODUCTS</subscript></h4> 
-        <p>
+      <div style={{paddingLeft:"10px"}}>
+      <h3 style={{marginTop:"20px"}}>OUR NUTRI FOOD </h3><h4 style={{marginTop:"20px"}}><subscript>YOUR DAILY NEED PRODUCTS</subscript></h4> 
+        <p style={{marginTop:"20px"}}>
         
         Greetings from Nutri Pure. Our software is committed to educating you about the truth behind the trends so you can concentrate on improvement rather than perfection. 
         Delicious healthy meal planned by our website that provides guidance on how to choose healthy diets to burn calories.
@@ -24,8 +26,8 @@ const About = () => {
         </p><br></br>
         <p>
         We serve as a dependable source of current knowledge on all aspects of fitness and nutrition that are important to your health and vitality. 
-        We put a lot of effort into developing and perfecting our 500+ collection of well curated, thorough, fact-checked material.<br></br>
-        Fruit is often low in calories and high in fibre, both of which may aid in weight loss.
+        We put a lot of effort into developing and perfecting our 500+ collection of well curated, thorough, fact-checked material.</p><br></br>
+        <p>Fruit is often low in calories and high in fibre, both of which may aid in weight loss.
         As we approach our centennial, we see numerous opportunities in our industry that build on our strengths in the areas of education and professional development, science policy and advocacy, nutrition science-based information and practise, and member engagement to become the leading authority on all things nutrition.
         
         </p>
@@ -37,7 +39,7 @@ Our website is continuously updated to stay current.
 Our website gets content recommendations from the foods that contain the most calories.
 The goal of the Nutri Pure Review Board is to provide you with practical, science-based recommendations that will motivate you to make healthy daily choices.
 </p>
-        
+</div>
         
         
         </div>
